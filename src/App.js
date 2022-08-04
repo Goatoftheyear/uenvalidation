acf0ee0a8ed7d28fc0128d2744c96622d3dcaa2b
Goatoftheyear <47184040+Goatoftheyear@users.noreply.github.com>
2022-08-04 22:52:59 +0800

    logic and simple test current the UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const ENTITY_TYPE = [
+  "LP",
+  "LL",
+  "FC",
+  "PF",
+  "RF",
+  "MQ",
+  "MM",
+  "NB",
+  "CC",
+  "CS",
+  "MB",
+  "FM",
+  "GS",
+  "DP",
+  "CP",
+  "NR",
+  "CM",
+  "CD",
+  "MD",
+  "HS",
+  "VH",
+  "CH",
+  "MH",
+  "CL",
+  "XL",
+  "CX",
+  "HC",
+  "RP",
+  "TU",
+  "TC",
+  "FB",
+  "FN",
+  "PA",
+  "PB",
+  "SS",
+  "MC",
+  "SM",
+  "GA",
+  "GB",
+];
+const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+  const submittingForm = (message) => {
+    if (
+      message.length < 9 ||
+      message.length > 10 ||
+      /[a-zA-Z]$/.test(message) === false
+    ) {
+      setResult("This UEN has invalid length");
+      return;
+    }
+    if (
+      /[TSR][0-9]{2}[A-Z]{2}[0-9]{4}[A-Z]/.test(message) &&
+      ENTITY_TYPE.includes(message.substring(3, 5))
+    ) {
+      //add the list of entities later
+      setResult("This is under other entities");
+    } else if (
+      /[0-9]{9}[A-Z]$/.test(message) &&
+      parseInt(message.substring(0, 4)) > 1800 &&
+      parseInt(message.substring(0, 4)) <= new Date().getFullYear()
+    ) {
+      setResult("Local companies registered with ACRA");
+    } else if (/[0-9]{8}[A-Z]$/.test(message) && message.length === 9) {
+      setResult("Businesses registered with ACRA");
+    } else {
+      setResult("This UEN is invalid");
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Type in your UEN to check if it is valid</h1>
+        <input
+          type="text"
+          name="uen"
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <br />
+        <button onClick={(e) => submittingForm(text)}>Submit</button>
+        <br />
+        <h2>{result}</h2>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders if render things are on the screen", () => {
+  const screen = render(<App />);
+  expect(screen.getByText("Type in your UEN to check if it is valid"));
+  expect(
+    screen.getByRole("textbox", {
+      name: "",
+    })
+  );
+  expect(screen.getByRole("button", { name: "Submit" }));
 });
+
+// test("type", () => {
+//   const screen = render(<App />);
+
+//   userEvent.type(screen.getByRole("input"), "Hello,{enter}World!");
+// });
